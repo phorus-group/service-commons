65be969f62b579f1dae7d4309a3fc019bfb9a5df
Martin Ivan Rios <martin.rios@doubleverify.com>
2024-05-25 05:24:39 +0300

    Set test profile - [skip ci]

--- src/test/kotlin/group/phorus/service/commons/bdd/CucumberSpringConfiguration.kt
+++ src/test/kotlin/group/phorus/service/commons/bdd/CucumberSpringConfiguration.kt
@@ -4,10 +4,12 @@ import io.cucumber.spring.CucumberContextConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(classes = [TestApp::class])
 @CucumberContextConfiguration
 @AutoConfigureWebTestClient
+@ActiveProfiles("test")
 class CucumberSpringConfiguration
 
 @SpringBootApplication(scanBasePackages = ["group.phorus"])
