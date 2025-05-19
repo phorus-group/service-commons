ad23e4f37e8d8fb50ff01a93780450c25f5d15b2
Martin Rios <irios@phorus.group>
2025-05-19 10:51:25 -0300

    bump version to 1.2.4 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 group = "group.phorus"
 description = "Library containing common logic for Spring based services."
-version = "1.2.3"
+version = "1.2.4"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
