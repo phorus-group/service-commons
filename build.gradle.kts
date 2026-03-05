a79662fa641132ec7e2fca9a09a94b55aadf0e40
Martin Rios <irios@phorus.group>
2026-03-05 13:55:23 +0200

    Fix vulnerabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ plugins {
     jacoco
 }
 
+ext["jackson-2-bom.version"] = "2.21.1"
+ext["jackson-bom.version"] = "3.1.0"
+
 group = "group.phorus"
 description = "Library containing common logic for Spring based services."
 version = "1.2.4"
